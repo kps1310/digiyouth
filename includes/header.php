54a9d0fdb0d8d65931f15eab6769f76000158454
Prabhat Sharma <prabhat.04011998@gmail.com>
2020-03-21 15:42:13 +0530

    navbar fixed

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,20 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>DIGIYOUTH INFOTECH -No.1 Digital Marketing School-Agency In Delhi & Gurgaon </title>
-	<meta name="description" content="DIGIYOUTH INFOTECH - Top Ranked Digital marketing School|Agency in Delhi-Gurgaon. Book a free demo class with us. 25+ Modules, Live Projects, 12+ Google Cerificates and many more." />
-	
-	<meta name="keywords" content="DIGIYOUTH INFOTECH, Digital marketing school delhi, Digital marketing agency Delhi, DIGIYOUTH INFOTECH COURSES,digital marketing course in delhi, internet marketing course in delhi, online marketing course in delhi, Digital marketing institute in delhi, digital marketing training in delhi, internet marketing institute in delhi, internet marketing training in delhi, best digital marketing institute in delhi, digital marketing institute near me, top 10 digital marketing institute in delhi, digital marketing training institute in delhi, career in digital marketing, digital marketing training institute near me, best digital marketing institute in delhi." />
-
-	<meta name="viewport" content="width=device-width , initial-scale=1" />
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css">
+  <title>DIGIYOUTH INFOTECH -No.1 Digital Marketing School-Agency In Delhi & Gurgaon </title>
+  <meta name="description" content="DIGIYOUTH INFOTECH - Top Ranked Digital marketing School|Agency in Delhi-Gurgaon. Book a free demo class with us. 25+ Modules, Live Projects, 12+ Google Cerificates and many more." />
+  <meta name="keywords" content="DIGIYOUTH INFOTECH, Digital marketing school delhi, Digital marketing agency Delhi, DIGIYOUTH INFOTECH COURSES,digital marketing course in delhi, internet marketing course in delhi, online marketing course in delhi, Digital marketing institute in delhi, digital marketing training in delhi, internet marketing institute in delhi, internet marketing training in delhi, best digital marketing institute in delhi, digital marketing institute near me, top 10 digital marketing institute in delhi, digital marketing training institute in delhi, career in digital marketing, digital marketing training institute near me, best digital marketing institute in delhi." />
+  <meta name="viewport" content="width=device-width , initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css">
   <link rel="shortcut icon" href="img/logo.png" />
-<meta name="google-site-verification" content="1WKQ79YhkUVyvDbDdiYLVCHRDt2mHbAJE33HlCktN5Y" />
+  <meta name="google-site-verification" content="1WKQ79YhkUVyvDbDdiYLVCHRDt2mHbAJE33HlCktN5Y" />
 <link rel="stylesheet" href="css/iconmoon.css">
 <link rel="stylesheet" type="text/css" href="css/digiyouth.css">
 <link rel="stylesheet" type="text/css" href="https://www.skillcircle.in/css/animate.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css">
-	<script type="text/javascript" src="js/main.js"></script>
-
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css">
+  <script type="text/javascript" src="js/main.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -32,13 +28,28 @@
 
   gtag('config', 'UA-155747085-1');
 </script>
+<style>
+  .navbar-brand-centered {
+        position: absolute;
+        left: 45%;
+        display: block;
+        width: 160px;
+        text-align: center;
+        background-color: transparent;
+        margin-top:-5px;
+    }
+    .navbar>.container .navbar-brand-centered, 
+    .navbar>.container-fluid .navbar-brand-centered {
+        margin-left: -80px;
+    } 
+</style>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
-	<script type="text/javascript">
-		
-
+<script type="text/javascript">
     $(document).ready(function() {
-     
       $("#owl-demo").owlCarousel({
      
           navigation : true, // Show next and prev buttons
@@ -55,30 +66,115 @@
       });
      
     });
-
-
-	</script>
+</script>
 <style>
+  .navbar {
+  min-height: 80px;
+  border-radius: 0px;
+  
+}
+pt{
+  overflow:auto;
+}
+.navbar-brand {
+  padding: 15px 15px;
+  height: 80px;
+  line-height: 80px;
+  z-index:999;
+}
+.navbar-toggle {
+  /* (80px - button height 34px) / 2 = 23px */
+  margin-top: 23px;
+  padding: 9px 10px !important;
+}
+.well{
+  margin-top:20px;
+  width:100%;
+  border-radius: 0px;
+  padding:0px;
+  border:none;
+}
+@media only screen and (min-width: 992px){
+  br{display:none;}
+}
+@media only screen and (max-width: 767px){
+br{display:none;}
+}
+
+@media only screen and (max-width: 991px) and (min-width: 768px){
+  .navbar-brand-centered {margin-top:15px;}
+}
 @media (min-width: 768px){
-    .navbar-nav{
-       margin-left:18em;
+.navbar-nav{
+      margin-top:10px;
+      /* margin-left:-70px; */
+}
+.dropdown:hover .dropdown-menu, .btn-group:hover .dropdown-menu{
+    display: block;
+} 
+nav.shift ul li a {
+  position:relative;
+  z-index: 1;
+}
+nav.shift ul li a:hover {
+  color: #91640F;
+}
+nav.shift ul li a:after {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+  width: 100%;
+  height: 1px;
+  content: '.';
+  color: transparent;
+  background: white;
+  visibility: none;
+  opacity: 0;
+  z-index: -1;
+}
+nav.shift ul li a:hover:after {
+  opacity: 1;
+  visibility: visible;
+  height: 100%;
+}
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $(".dropdown, .btn-group").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});     
+</script>
+.navbar-nav li>a{
+    padding-top: 26.5px;
+    padding-bottom: 26.5px;
+    line-height: 27px;
+}
+.dropdown li a {
+         padding: 6px 6px;
     }
+}
+.navbar-default,.dropdown-menu{
+    background:cadetblue;
 }
 .navbar-nav li>a{
     color:black !important;
     font-size:16px !important;
 }
-.navbar-default{
-    background:cadetblue;
-}
 </style>
-
 <header id="home">
 
 <!-- upper bar -->
 
 <div class="upperHeader" style="height: 40px">
-    
+
     <div style="text-align: left;">
       <span>
       <ul style="text-align:left;display: inline;">
@@ -94,91 +190,59 @@
       <li style="float:right; padding: 10px" ><a href="./contact.php" style="text-decoration: none;color:inherit;">CONTACT US</a></li>
     </ul>
   </span>
-    
     </div>
-
   </div>
 
-
-<!-- navbar starting -->
-
-<!--<nav id="nav" class="navbar nav-transparent navbar-dark bg-dark" style="background: #000;">-->
-<!--<div class="container">-->
-<!--<div class="navbar-header">-->
-
-
-
-<!--<div class="nav-collapse" style="background:white;">-->
-<!--<span></span>-->
-
-<!--</div>-->
-
-<!--</div>-->
-
-<!--<ul class="main-nav nav navbar-nav " style="margin-left: 12em">-->
-<!--  <li><a  href="./">Home</a></li>-->
-<!--<li><a href="./#service">Digital Marketing Services</a></li>-->
-<!-- <li><a href="#portfolio">Portfolio</a></li> -->
-<!--<li><a href="index.php"><img class="logo" src="img/logo.png" alt="logo"></a></li>-->
-<!-- <li><a href="#pricing">Prices</a></li> -->
-<!--<li class="has-dropdown"><a href="./#courses">Digital Marketing Courses </a>-->
-<!--<ul class="dropdown" style="position:absolute;">-->
-<!--<li><a href="./course-search-engine-optimization.php">SEO</a></li>-->
-<!--<li><a href="./course-content-marketing.php">Content Marketing</a></li>-->
-<!--<li><a href="./course-web-development.php">Web Development</a></li>-->
-<!--</ul>-->
-<!--</li>-->
-<!--<li><a href="register.php">Register</a></li>-->
-<!--</ul>-->
-
-<!--</div>-->
-<!--</nav>-->
-
-
- <div class="navbar-wrapper">
-      <div class="">
-
-        <div class="navbar navbar-default navbar-static-top" role="navigation">
-        
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>  
-                <span class="icon-bar"></span>
-              </button>
-             
-            </div>
-            <div class="collapse navbar-collapse">
-              <ul class="nav navbar-nav ">
-                  <li><a  href="./"><b>HOME</b></a></li>
-
-<li class="has-dropdown"><a href="./#service"><b>DIGITAL MARKETING COURSES </b></a>
-<ul class="dropdown" style="position:absolute;">
-<li><a href="./course-search-engine-optimization.php">SEO</a></li>
-<li><a href="./course-content-marketing.php">Content Marketing</a></li>
-<li><a href="./course-web-development.php">Web Development</a></li>
-<li><a href="./course-influencer-marketing.php">Influencer Marketing</a></li>
-<li><a href="./course-display-advertising.php">Display Advertising</a></li>
-</ul>
-</li>
- <!--<li><a href="#portfolio">Portfolio</a></li> -->
-<li><a href="index.php"><img class="logo" src="img/Webp.net-resizeimage (1).png" alt="logo"></a></li>
- <!--<li><a href="#pricing">Prices</a></li> -->
-<li class="has-dropdown"><a href="./#courses"><b>DIGITAL MARKETING SERVICES </b></a>
-<ul class="dropdown" style="position:absolute;">
-<li><a href="./search-engine-optimization.php">SEO</a></li>
-<li><a href="./social-media-marketing.php">SMM</a></li>
-<li><a href="./web-designing-development.php">Web Development</a></li>
-<li><a href="./business-branding.php">BUSINESS BRANDING</a></li>
-<li><a href="./ppc-branding.php">PPC SERVICES</a></li>
-
-</ul>
-</li>
-<li><a href="register.php"><b>REGISTER </b></a></li>
-              </ul>
-            </div>
-          
+<!--New Navbar Begins from here-->
+        <nav class="navbar navbar-default shift" role="navigation">
+        <div class="container well">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="navbar-brand navbar-brand-centered" style="position:absolute;"><a href="index.php"><img class="logo" src="img/Webp.net-resizeimage (1).png" alt="logo"></a></div>
         </div>
 
-      </div>
-    </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-brand-centered">
+          <ul class="nav navbar-nav shift">
+            <li ><a  href="./ "><b>HOME</b></a></li>
+            <li class="dropdown">
+                  <a href="./#services" class="dropdown-toggle" data-toggle="dropdown"><b><pt>DIGITAL <br/>MARKETING <br/>COURSES</pt></b><span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="lessht"><a href="./course-search-engine-optimization.php">SEO</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./course-content-marketing.php">Content Marketing</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./course-web-development.php">Web Development</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./course-influencer-marketing.php">Influencer Marketing</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./course-display-advertising.php">Display Advertising</a></li>
+                </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right shift">
+            <li class="dropdown">
+                  <a href="./#" class="dropdown-toggle" data-toggle="dropdown"><b><pt>DIGITAL <br/>MARKETING <br/>SERVICES</pt></b><span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                  <li class="lessht"><a href="./search-engine-optimization.php">SEO</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./social-media-marketing.php">SMM</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./web-designing-development.php">Web Development</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./business-branding.php">BUSINESS BRANDING</a></li>
+                    <li class="divider"></li>
+                    <li class="lessht"><a href="./ppc-branding.php">PPC SERVICES</a></li>
+                  </ul>
+                </li>    
+                <li><a href="register.php"><b>REGISTER </b></a></li>      
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>

2a91c5d5ac1feeddcdcf080d4f22727bb595ecbb
kps1310 <47588874+kps1310@users.noreply.github.com>
2020-03-23 20:23:29 +0530

    new UI

--- includes/header.php
+++ includes/header.php
@@ -46,8 +46,123 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+<!-- header contents for modules section -->
+<style id="rocket-critical-css">
+                    .wp-block-media-text .wp-block-media-text__media{grid-area:media-text-media;margin:0}
+                    .wp-block-media-text .wp-block-media-text__content
+                    {word-break:break-word;grid-area:media-text-content;padding:0 8%}
+                    .panel-grid.panel-has-style>.panel-row-style
+                    {display:flex;-ms-flex-wrap:wrap;flex-wrap:nowrap;-ms-justify-content:space-between;justify-content:space-between}
+                    .panel-grid-cell{-ms-box-sizing:border-box;box-sizing:border-box}.panel-grid-cell .panel-cell-style{height:100%}
+                    .panel-grid-cell .so-panel{zoom:1}.panel-grid-cell .so-panel:before{content:"";display:block}
+                    .panel-grid-cell .so-panel:after{content:"";display:table;clear:both}
+                    .panel-grid-cell .panel-last-child{margin-bottom:0}
+                    body.siteorigin-panels-before-js{overflow-x:hidden}
+                    div.wpforms-container-full,div.wpforms-container-full .wpforms-form 
+                    *{background:none;border:0 none;border-radius:0;-webkit-border-radius:0;-moz-border-radius:0;
+                    float:none;font-size:100%;height:auto;letter-spacing:normal;
+                    list-style:none;outline:none;position:static;text-decoration:none;
+                    text-indent:0;text-shadow:none;text-transform:none;width:auto;visibility:visible;
+                    overflow:visible;margin:0;padding:0;box-sizing:border-box;-webkit-box-sizing:border-box;
+                    -moz-box-sizing:border-box;-webkit-box-shadow:none;-moz-box-shadow:none;-ms-box-shadow:none;
+                    -o-box-shadow:none;box-shadow:none}
+                    
+                    .btn-primary{color:#fff;background-color:#337ab7;border-color:#2e6da4}
+                    
+                    a{color:#337ab7;text-decoration:none}
+                    img{vertical-align:middle}
+                    
+                    p{margin:0 0 10px}
+                    .text-center{text-align:center}
+                    ul{margin-top:0;margin-bottom:10px}
+                    ul ul{margin-bottom:0}
+                    .container{margin-right:auto;margin-left:auto;padding-left:15px;padding-right:15px}
+                    @media (min-width:768px){.container{width:750px}}
+                    @media (min-width:992px){.container{width:970px}}
+                    @media (min-width:1200px){.container{width:1170px}}
+                    .row{margin-left:-15px;margin-right:-15px}.col-sm-12{position:relative;min-height:1px;padding-left:15px;padding-right:15px}
+                    @media (min-width:768px){.col-sm-12{float:left}.col-sm-12{width:100%}}
+                    label{display:inline-block;max-width:100%;margin-bottom:5px;font-weight:700}
+                    .btn{display:inline-block;margin-bottom:0;font-weight:400;text-align:center;
+                    vertical-align:middle;touch-action:manipulation;background-image:none;
+                    border:1px solid transparent;white-space:nowrap;padding:6px 12px;font-size:14px;
+                    line-height:1.42857143;border-radius:4px}
+
+                                     
+                    .container:before,.container:after,.row:before,.row:after
+                    .container:after,.row:after,.container:before,.container:after,.row:before,.row:after
+                    {content:" ";display:table}.container:after,.row:after{clear:both}*{margin:0;padding:0}
+                    img{max-width:100%;height:auto}ul{margin-bottom:10px;margin-left:25px}
+                    ul ul{margin:4px 0 5px 30px}ul ul li{margin-bottom:6px}.animated{-webkit-animation-duration:1s;animation-duration:1s;
+                    -webkit-animation-fill-mode:both;animation-fill-mode:both}.animated.infinite
+                    {-webkit-animation-iteration-count:infinite;animation-iteration-count:infinite}
+                    @-webkit-keyframes tada{0%{-webkit-transform:scale(1);transform:scale(1)}10%,20%{-webkit-transform:scale(.9) rotate(-3deg);
+                    transform:scale(.9) rotate(-3deg)}
+                    30%,50%,70%,90%{-webkit-transform:scale(1.1) rotate(3deg);transform:scale(1.1) rotate(3deg)}
+                    40%,60%,80%{-webkit-transform:scale(1.1) rotate(-3deg);transform:scale(1.1) rotate(-3deg)}
+                    100%{-webkit-transform:scale(1) rotate(0);transform:scale(1) rotate(0)}}
+                    @keyframes tada{0%{-webkit-transform:scale(1);-ms-transform:scale(1);transform:scale(1)}
+                    10%,20%{-webkit-transform:scale(.9) rotate(-3deg);-ms-transform:scale(.9) rotate(-3deg);transform:scale(.9) rotate(-3deg)}
+                    30%,50%,70%,90%{-webkit-transform:scale(1.1) rotate(3deg);-ms-transform:scale(1.1) rotate(3deg);transform:scale(1.1) rotate(3deg)}
+                    40%,60%,80%{-webkit-transform:scale(1.1) rotate(-3deg);-ms-transform:scale(1.1) rotate(-3deg);transform:scale(1.1) rotate(-3deg)}
+                    100%{-webkit-transform:scale(1) rotate(0);-ms-transform:scale(1) rotate(0);transform:scale(1) rotate(0)}}
+                    .tada{-webkit-animation-name:tada;animation-name:tada}
+                    body{overflow-x:hidden}
+                    button{line-height:30px;padding:5px 25px;border:0;box-shadow:none;border-radius:0;font-size:13px;font-weight:700;text-transform:uppercase}
+                    input,button,select{border:1px solid #eee;padding:5px 10px;box-shadow:none;text-shadow:none}
+                    button{padding:5px 25px;text-transform:uppercase;font-size:13px;font-weight:700;line-height:30px;border:0}
+                    @media (min-width:1200px){.container{width:1200px}} 
+                    @media (max-width:1024px){.container{width:1024px;max-width:100%}}
+                    @media (max-width:992px){.container{width:960px;max-width:100%}}
+                    @media (min-width:768px) and (max-width:959px){.container{width:900px;max-width:100%}}
+                    @media (max-width:767px){.site-header{padding:15px 0}#masthead.affix-top{margin-top:0}
+                    
+                    body .content-pusher{background-color:#fff}body #main-content{background-color:#fff}
+                    
+                    
+                    @media screen and (max-width:768px) and (min-width:320px){html{margin-bottom:32px!important}
+                    
+        </style>
+
+
+        <style type="text/css" media="all" id="siteorigin-panels-layouts-head">
+                    #pgc-9227-0-0,#pgc-9227-1-0,#pgc-9227-2-0,#pgc-9227-4-0,#pgc-9227-6-0,#pgc-9227-7-0,#pgc-9227-8-0,#pgc-9227-9-0,#pgc-9227-10-0
+                    {width:100%;width:calc(100% - (0 * 30px))}
+                    #pg-9227-0,#pg-9227-2,#pg-9227-3,#pg-9227-4,#pg-9227-5,#pg-9227-6,#pg-9227-7,#pg-9227-8,#pl-9227 .so-panel
+                    {margin-bottom:30px}#pg-9227-1,#pg-9227-9,#pl-9227 .so-panel:last-child{margin-bottom:0}
+                    #pgc-9227-3-0{width:100%}
+                    #pgc-9227-5-0,#pgc-9227-5-1,#pgc-9227-5-2,#pgc-9227-5-3{width:25%;width:calc(25% - (0.75 * 30px))}
+                    #pg-9227-3.panel-no-style,#pg-9227-3.panel-has-style>.panel-row-style,
+                    {-webkit-align-items:flex-start;align-items:flex-start}
+                    #panel-9227-3-0-0>.panel-widget-style
+                    {padding:10px 10px 10px 10px}
+                    #panel-9227-4-0-2>.panel-widget-style
+                    {text-align:center}#pg-9227-5>.panel-row-style
+                    {padding:124px 0}
+                    #pg-9227-9>.panel-row-style{padding-bottom:85px;background-position:right bottom}
+                    @media (max-width:780px){
+                    #pg-9227-3.panel-no-style,#pg-9227-3.panel-has-style>.panel-row-style
+                    {-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column}
+                    #pg-9227-3>.panel-grid-cell,#pg-9227-3>.panel-row-style>.panel-grid-cell,
+                    {width:100%;margin-right:0}#pgc-9227-5-0,#pgc-9227-5-1,#pgc-9227-5-2{margin-bottom:30px}
+                    #pl-9227 .panel-grid-cell{padding:0}#pl-9227 .panel-grid .panel-grid-cell-empty{display:none}
+                    #pl-9227 .panel-grid .panel-grid-cell-mobile-last{margin-bottom:0}#pg-9227-2>.panel-row-style{padding:0 10px 0 10px}}
+                   
+        </style>
+
+
+                    <!-- IMP -->
+                    <link data-minify="1" rel="stylesheet" href="https://192841-578335-raikfcquaxqncofqfm.stackpathdns.com/wp-content/cache/min/1/wp-content/themes/eduma/custom-css-ims-50161bf5e620a1becc0bbee50fc8cfb5.css" as="style" onload="this.onload=null;this.rel='stylesheet'" type="text/css" media="all">
+                    <link data-minify="1" rel="stylesheet" href="https://192841-578335-raikfcquaxqncofqfm.stackpathdns.com/wp-content/cache/min/1/wp-content/themes/eduma/custom-css-2-e9b1d9d163dec9327e89b18845516850.css" as="style" onload="this.onload=null;this.rel='stylesheet'" type="text/css" media="all">
+        
+<!-- modules content end here -->
 </head>
-<body>
+
+
+
+<body data-rsssl="1" class="home page-template page-template-page-templates page-template-homepage page-template-page-templateshomepage-php page page-id-9227  siteorigin-panels  siteorigin-panels-home group-blog ims-main-body" id="thim-body">
 <script type="text/javascript">
     $(document).ready(function() {
       $("#owl-demo").owlCarousel({
